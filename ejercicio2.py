e247b6fd043136f776ec2d9a0508ba0e53057132
daguzman35 <da.guzman@outlook.com>
2018-06-18 16:00:14 -0500

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -4,3 +4,15 @@
 # La ecuación debe ser resuelta hasta un tiempo final T=0.5 y una velocidad c=-1.0.
 # El programa debe hacer una gráfica (adveccion.png) con la condición incial y el resultado final.
 
+import numpy as np
+
+tfin=0.5
+c=-1.0
+
+miT=np.linspace(0,tfin)
+miX=np.linspace(-10,10)
+
+def miecu(dt,dx,u,c):
+    return dt*u+c*dx
+  
+#Son las 3:59pm
